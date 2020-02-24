c9070b1d069fe64b6a425f582dcb693e9471735c
Mike Heft <mikeheft@gmail.com>
2020-02-24 12:29:58 -0400

    Add ability to parse json to ensure it is not being double encoded

--- lib/dry_serialization/blueprinter.rb
+++ lib/dry_serialization/blueprinter.rb
@@ -3,7 +3,7 @@ require 'blueprinter'
 module DrySerialization::Blueprinter
 
   def serialized_resource(resource, blueprint, options = {})
-    blueprint.render(resource, options)
+    JSON.parse(blueprint.render(resource, options), symbolize_names: true)
   end
   
 end
