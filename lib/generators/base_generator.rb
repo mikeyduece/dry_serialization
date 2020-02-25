580ced5f7d6596fcf8a05509f75af5c4e004bf71
Mike Heft <mikeheft@gmail.com>
2020-02-25 08:32:32 -0400

    Refactor to dry up install generator in prep for support of other serializer gems

--- lib/generators/base_generator.rb
+++ lib/generators/base_generator.rb
@@ -6,13 +6,24 @@ class BaseGenerator < Rails::Generators::Base
     true
   end
 
+  def remove_other_supported_gems(*gems)
+    gems = [gems] unless gems.is_a?(Array)
+    return if gems.empty?
+    
+    puts set_color('Removing other supported gems', :orange)
+    gems.each do |gem|
+      run "gem uninstall #{gem.underscore}"
+      log_statement(gem)
+    end
+  end
+
   def copy_api_controller
     unless File.exists?('app/controllers/api_controller.rb')
       puts 'ApiController not present, creating ApiController...'
       template 'api_controller.rb', 'app/controllers/api_controller.rb'
     end
   end
-  
+
   private
 
   def log_statement(serializer, color = :green)
--- lib/generators/dry_serialization/blueprinter/install_generator.rb
+++ lib/generators/dry_serialization/blueprinter/install_generator.rb
@@ -5,28 +5,21 @@ module DrySerialization
     class InstallGenerator < BaseGenerator
       source_root File.expand_path("../../../templates", __FILE__)
 
-      def remove_other_supported_gems
-        puts set_color('Removing other supported gems', :orange)
-        run 'gem uninstall active_model_serializers'
-        log_statement('ActiveModelSerializers')
-        run 'gem uninstall fast_jsonapi'
-        log_statement('FastJsonapi')
-      end
-      
       def install_blueprinter
+        remove_other_supported_gems('ActiveModelSerializers', 'Fast_Jsonapi')
         puts 'Installing Blueprinter...'
         run 'gem install blueprinter'
       end
-     
-      
-      def include_helper
+
+
+      def helper_include
         puts 'Adding include statement to ApiController'
         insert_into_file('app/controllers/api_controller.rb',
                          "\n\tinclude DrySerialization::Blueprinter",
                          after: 'class ApiController < ActionController::API'
-                         )
+        )
       end
-      
+     
     end
   end
 end
