1e2d3cc8a1f6c2ba4264156c80ee13b68e3205c2
Mike Heft <mikeheft@gmail.com>
2020-02-26 11:19:40 -0400

    Add generator for fatjson

--- lib/generators/base_generator.rb
+++ lib/generators/base_generator.rb
@@ -2,13 +2,18 @@ require 'rails/generators/base'
 
 class BaseGenerator < Rails::Generators::Base
   API_CONTROLLER_PATH = 'app/controllers/api_controller.rb'.freeze
+  SERIALIZERS = {
+    ams: 'ActiveModelSerializers',
+    blueprinter: 'Blueprinter',
+    fast_jsonapi: 'FastJsonapi'
+  }
   
   def self.exit_on_failure?
     true
   end
   
   private
-
+  
   # Removes other serialization gems, currently just AMS and FastJsonapi
   def remove_other_supported_gems(*gems)
     gems = [gems] unless gems.is_a?(Array)
@@ -18,19 +23,30 @@ class BaseGenerator < Rails::Generators::Base
       log_statement(gem, :yellow)
       gsub_file('Gemfile', /^(\ngem #{gem.underscore})/, '')
     end
-    
+  
   end
-
+  
   # Adds an ApiController if one does not exist to ensure proper encapsulation,
   # especially if in a Rails monolith with Rails HTML views as well
   def copy_api_controller
     puts set_color('Checking if an ApiController exists...', :cyan)
+    
     unless File.exists?('app/controllers/api_controller.rb')
       puts set_color('ApiController not present, creating ApiController...', :cyan)
       template 'api_controller.rb', API_CONTROLLER_PATH
     end
   end
-
+  
+  def helper_include(serializer)
+    copy_api_controller
+    gsub_file(API_CONTROLLER_PATH, /^*(include DrySerialization::.*)\n/, '')
+    puts 'Adding include statement to ApiController'
+    insert_into_file(API_CONTROLLER_PATH,
+                     "\n\tinclude DrySerialization::#{serializer}",
+                     after: 'class ApiController < ActionController::API'
+    )
+  end
+  
   def log_statement(serializer, color = :green)
     puts set_color("Removed #{serializer}", color)
   end
--- lib/generators/dry_serialization/fast_jsonapi/install_generator.rb
+++ lib/generators/dry_serialization/fast_jsonapi/install_generator.rb
@@ -1,21 +1,21 @@
 require 'generators/base_generator'
 
 module DrySerialization
-  module Blueprinter
+  module FastJsonapi
     class InstallGenerator < BaseGenerator
       source_root File.expand_path("../../../templates", __FILE__)
       
       # Add blueprinter gem to gemfile after dry_serialization declaration and bundles the newly declared gem
       def install_blueprinter
-        remove_other_supported_gems(SERIALIZERS[:ams], SERIALIZERS[:fast_jsonapi])
+        remove_other_supported_gems(SERIALIZERS[:blueprinter], SERIALIZERS[:ams])
         
         puts "Installing #{SERIALIZERS[:blueprinter]}..."
         insert_into_file('Gemfile',
-                         "\ngem 'blueprinter'",
+                         "\ngem 'fast_jsonapi'",
                          after: "gem 'dry_serialization', source: 'https://gem.fury.io/mikeyduece-gems/'")
         run 'bundle install'
         
-        helper_include(SERIALIZERS[:blueprinter])
+        helper_include(SERIALIZERS[:fast_jsonapi])
       end
     
     end
