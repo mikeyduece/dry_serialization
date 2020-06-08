c0db32bd10e68b558429fe4cb02f1ed3925ae199
Mike Heft <mikeheft@gmail.com>
2020-06-08 14:17:28 -0600

    Bummp minor version

--- lib/dry_serialization/blueprinter.rb
+++ lib/dry_serialization/blueprinter.rb
@@ -4,6 +4,6 @@ module DrySerialization
     def serialized_resource(resource, blueprint, options = {})
       JSON.parse(blueprint.render(resource, options), symbolize_names: true)
     end
-    
+
   end
 end
