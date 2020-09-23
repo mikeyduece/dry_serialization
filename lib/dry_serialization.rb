378a7560b3a57f4a25dc8e72f6d82ad5dee340dd
Mike Heft <mikeheft@gmail.com>
2020-09-23 08:49:43 -0600

    Add require statement, bump version minor minor

--- lib/dry_serialization.rb
+++ lib/dry_serialization.rb
@@ -1,6 +1,7 @@
 require "dry_serialization/version"
 require "dry_serialization/blueprinter"
 require "dry_serialization/fast_jsonapi"
+require "dry_serialization/concerns/serialization_helper"
 
 module DrySerialization
   
