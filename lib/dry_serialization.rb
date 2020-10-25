36d46ecddee5a723e36ef2ec96a318b91ce7380c
Mike Heft <mikeheft@gmail.com>
2020-10-25 10:06:47 -0600

    Fix require statement for deserialization

--- lib/dry_serialization.rb
+++ lib/dry_serialization.rb
@@ -2,6 +2,7 @@ require "dry_serialization/version"
 require "dry_serialization/blueprinter"
 require "dry_serialization/jsonapi_serializer"
 require "dry_serialization/concerns/serialization_helper"
+require "dry_serialization/concerns/deserializable"
 
 module DrySerialization
   
